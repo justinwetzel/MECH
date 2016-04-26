393dcbfa2b03eff5d63199c8737aa6482bf085ca
justinwetzel <justinwetzel@mail.usf.edu>
2016-04-26 18:49:15 -0400

    Testing git

--- MECHClubApp/AddPart.Designer.cs
+++ MECHClubApp/AddPart.Designer.cs
@@ -15,7 +15,7 @@
         {
             if (disposing && (components != null))
             {
-                components.Dispose();
+                components.Dispose(); //testing github push
             }
             base.Dispose(disposing);
         }
